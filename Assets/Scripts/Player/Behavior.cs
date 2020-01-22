b9d82ae9ecb9108ef4b6e2b6cd200fefa1d9a4c5
arudomanenko <rudomanenkoanton@gmail.com>
2020-01-22 16:44:20 +0300

    feat: raw input system;

---
+++ Assets/Scripts/Player/Behavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using InputSystem;
+
+namespace Player
+{
+    public class Behavior : MonoBehaviour
+    {
+        public PlayerInput playerInput;
+        public float forceFactorX = 10;
+        public float forceFactorY = 10;
+
+        private Rigidbody2D _rigidbody2D;
+        
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void OnEnable()
+        {
+            playerInput.onInputDetected.AddListener(Jump);
+        }
+
+        private void OnDisable()
+        {
+            playerInput.onInputDetected.RemoveListener(Jump);
+        }
+
+        private void Jump()
+        {
+            _rigidbody2D.AddForce(new Vector2(forceFactorX, forceFactorY), ForceMode2D.Impulse );
+        }
+    }
+}
---
+++ Assets/Scripts/Player/InputSystem/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace InputSystem
+{
+    public class PlayerInput : MonoBehaviour
+    {    
+        public UnityEvent onInputDetected = new UnityEvent();
+        
+#if UNITY_EDITOR
+        private float downTime = .0f;
+        private float pressedRequiredTime = .4f;
+
+        private void Awake()
+        {
+                downTime = Time.time;                   
+        }
+#endif
+
+        private void FixedUpdate()
+        {
+#if UNITY_EDITOR
+            if (Input.GetKey(KeyCode.Space))
+            {
+                    if (Time.time - downTime > pressedRequiredTime)
+                    {
+                            onInputDetected.Invoke();
+                            downTime = Time.time;
+                    }
+            }
+#endif
+        }
+    }
+}
