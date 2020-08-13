379d0141056c140c49fcd5010244fc5ac107db3c
arudomanenko <rudomanenkoanton@gmail.com>
2020-08-13 12:01:01 +0300

    buildClass is added

---
+++ Assets/Editor/BuildClass.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildClass
+{
+    private static List<string> scenesNamesOp = new List<string>{"SampleScene"};
+    private static List<string> scenesNamesOf = new List<string>{"ToDeleteScene"};
+
+    public static void BuildMethod(string arg)
+    {
+        string[] scenes = AssetDatabase.FindAssets("t:Scene", null);
+        List<EditorBuildSettingsScene> editorBuildSettingsScenes = new List<EditorBuildSettingsScene>();
+        
+        foreach (var s in scenes)
+        {
+            editorBuildSettingsScenes.Add(new EditorBuildSettingsScene(s, true));
+        }
+        
+        if (arg == "op")
+        {
+            foreach (var es in editorBuildSettingsScenes)
+            {
+                string sceneName = Path.GetFileNameWithoutExtension(es.path);
+
+                es.enabled = scenesNamesOp.Contains(sceneName);
+            }
+        }
+        else if (arg == "of")
+        {
+            foreach (var es in editorBuildSettingsScenes)
+            {
+                string sceneName = Path.GetFileNameWithoutExtension(es.path);
+
+                es.enabled = scenesNamesOf.Contains(sceneName);
+            }
+        }
+        
+        EditorBuildSettings.scenes = editorBuildSettingsScenes.ToArray();
+    }
+}
